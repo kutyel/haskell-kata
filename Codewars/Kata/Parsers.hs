bde30daf28718eda7f35b22325a07ce29f8e9882
Flavio Corpa <flaviocorpa@gmail.com>
2024-04-02 14:46:00 +0200

    Add parser example for my blog 💴

---
+++ Codewars/Kata/Parsers.hs
@@ -0,0 +1,27 @@
+module Codewars.Kata.Parsers where
+
+import Control.Applicative ((<|>))
+import Text.Trifecta (Parser, char, runParser, stringLiteral, symbol)
+
+data Import
+    = Idl String
+    | Protocol String
+    | Schema String
+    deriving (Eq, Show, Ord)
+
+parser :: Parser Import
+parser =
+    symbol "import"
+        *> ( importHelper Idl "idl"
+                <|> importHelper Protocol "protocol"
+                <|> importHelper Schema "schema"
+           )
+  where
+    importHelper :: (String -> Import) -> String -> Parser Import
+    importHelper f s = f <$> (symbol s *> stringLiteral <* char ';')
+
+main :: IO ()
+main = do
+    print $ runParser parser mempty "import protocol \"foo.avpr\";"
+
+-- > Success (ProtocolImport "foo.avpr")
